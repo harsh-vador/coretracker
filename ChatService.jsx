1df6423dacc2e60402dd418d48640cbd645f1731
Harsh Vador <harsh.vador@somaiya.edu>
2024-10-19 14:44:51 +0530

    remove unwanted code

--- ChatService.jsx
+++ ChatService.jsx
@@ -19,158 +19,217 @@ export const getChatGPTResponse = async (userInput) => {
         );
         return response.data.choices[0].message.content;
     } catch (error) {
-        console.error('Error fetching ChatGPT response:', error);
         return dummyData;
     }
 };
 
-const dummyData = [{'Squats': 10,
+const dummyData = [{
+    'Squats': 10,
     'Push-ups': 10,
     'Planks': 10,
     'Lunges': 10,
-    'isComplete': false},
-   {'Squats': 10,
+    'isComplete': false
+},
+{
+    'Squats': 10,
     'Push-ups': 10,
     'Planks': 10,
     'Lunges': 10,
-    'isComplete': false},
-   {'Squats': 10,
+    'isComplete': false
+},
+{
+    'Squats': 10,
     'Push-ups': 10,
     'Planks': 10,
     'Lunges': 10,
-    'isComplete': false},
-   {'Squats': 10,
+    'isComplete': false
+},
+{
+    'Squats': 10,
     'Push-ups': 10,
     'Planks': 10,
     'Lunges': 10,
-    'isComplete': false},
-   {'Squats': 15,
+    'isComplete': false
+},
+{
+    'Squats': 15,
     'Push-ups': 15,
     'Planks': 15,
     'Lunges': 15,
-    'isComplete': false},
-   {'Squats': 15,
+    'isComplete': false
+},
+{
+    'Squats': 15,
     'Push-ups': 15,
     'Planks': 15,
     'Lunges': 15,
-    'isComplete': false},
-   {'Squats': 15,
+    'isComplete': false
+},
+{
+    'Squats': 15,
     'Push-ups': 15,
     'Planks': 15,
     'Lunges': 15,
-    'isComplete': false},
-   {'Squats': 15,
+    'isComplete': false
+},
+{
+    'Squats': 15,
     'Push-ups': 15,
     'Planks': 15,
     'Lunges': 15,
-    'isComplete': false},
-   {'Squats': 15,
+    'isComplete': false
+},
+{
+    'Squats': 15,
     'Push-ups': 15,
     'Planks': 15,
     'Lunges': 15,
-    'isComplete': false},
-   {'Squats': 20,
+    'isComplete': false
+},
+{
+    'Squats': 20,
     'Push-ups': 20,
     'Planks': 20,
     'Lunges': 20,
-    'isComplete': false},
-   {'Squats': 20,
+    'isComplete': false
+},
+{
+    'Squats': 20,
     'Push-ups': 20,
     'Planks': 20,
     'Lunges': 20,
-    'isComplete': false},
-   {'Squats': 20,
+    'isComplete': false
+},
+{
+    'Squats': 20,
     'Push-ups': 20,
     'Planks': 20,
     'Lunges': 20,
-    'isComplete': false},
-   {'Squats': 20,
+    'isComplete': false
+},
+{
+    'Squats': 20,
     'Push-ups': 20,
     'Planks': 20,
     'Lunges': 20,
-    'isComplete': false},
-   {'Squats': 20,
+    'isComplete': false
+},
+{
+    'Squats': 20,
     'Push-ups': 20,
     'Planks': 20,
     'Lunges': 20,
-    'isComplete': false},
-   {'Squats': 25,
+    'isComplete': false
+},
+{
+    'Squats': 25,
     'Push-ups': 25,
     'Planks': 25,
     'Lunges': 25,
-    'isComplete': false},
-   {'Squats': 25,
+    'isComplete': false
+},
+{
+    'Squats': 25,
     'Push-ups': 25,
     'Planks': 25,
     'Lunges': 25,
-    'isComplete': false},
-   {'Squats': 25,
+    'isComplete': false
+},
+{
+    'Squats': 25,
     'Push-ups': 25,
     'Planks': 25,
     'Lunges': 25,
-    'isComplete': false},
-   {'Squats': 25,
+    'isComplete': false
+},
+{
+    'Squats': 25,
     'Push-ups': 25,
     'Planks': 25,
     'Lunges': 25,
-    'isComplete': false},
-   {'Squats': 25,
+    'isComplete': false
+},
+{
+    'Squats': 25,
     'Push-ups': 25,
     'Planks': 25,
     'Lunges': 25,
-    'isComplete': false},
-   {'Squats': 30,
+    'isComplete': false
+},
+{
+    'Squats': 30,
     'Push-ups': 30,
     'Planks': 30,
     'Lunges': 30,
-    'isComplete': false},
-   {'Squats': 30,
+    'isComplete': false
+},
+{
+    'Squats': 30,
     'Push-ups': 30,
     'Planks': 30,
     'Lunges': 30,
-    'isComplete': false},
-   {'Squats': 30,
+    'isComplete': false
+},
+{
+    'Squats': 30,
     'Push-ups': 30,
     'Planks': 30,
     'Lunges': 30,
-    'isComplete': false},
-   {'Squats': 30,
+    'isComplete': false
+},
+{
+    'Squats': 30,
     'Push-ups': 30,
     'Planks': 30,
     'Lunges': 30,
-    'isComplete': false},
-   {'Squats': 30,
+    'isComplete': false
+},
+{
+    'Squats': 30,
     'Push-ups': 30,
     'Planks': 30,
     'Lunges': 30,
-    'isComplete': false},
-   {'Squats': 35,
+    'isComplete': false
+},
+{
+    'Squats': 35,
     'Push-ups': 35,
     'Planks': 35,
     'Lunges': 35,
-    'isComplete': false},
-   {'Squats': 35,
+    'isComplete': false
+},
+{
+    'Squats': 35,
     'Push-ups': 35,
     'Planks': 35,
     'Lunges': 35,
-    'isComplete': false},
-   {'Squats': 35,
+    'isComplete': false
+},
+{
+    'Squats': 35,
     'Push-ups': 35,
     'Planks': 35,
     'Lunges': 35,
-    'isComplete': false},
-   {'Squats': 35,
+    'isComplete': false
+},
+{
+    'Squats': 35,
     'Push-ups': 35,
     'Planks': 35,
     'Lunges': 35,
-    'isComplete': false},
-   {'Squats': 35,
+    'isComplete': false
+},
+{
+    'Squats': 35,
     'Push-ups': 35,
     'Planks': 35,
     'Lunges': 35,
-    'isComplete': false},
-   {'Squats': 40,
+    'isComplete': false
+},
+{
+    'Squats': 40,
     'Push-ups': 40,
     'Planks': 40,
     'Lunges': 40,
-    'isComplete': false}];
+    'isComplete': false
+}];
